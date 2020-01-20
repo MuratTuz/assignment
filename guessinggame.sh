c0a3686ae629700f8fdfe089ffffe32e4d2fb9d4
MuratTuz <murattuzlali@gmail.com>
2020-01-20 20:51:11 +0100

    number of files variable in .sh file is modified

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,34 +1,33 @@
 #This is simple game programmed by bash script
 
-export PATH=$PWD:$PATH
+echo "export PATH=$PWD:$PATH" >>~/.bash_profile
 
 function files {
-number_of_Files=$(ls -a | grep -v "\.$" | wc -l)
-echo $number_of_Files
+    number_of_Files=$(ls -al | grep -v "^d" |  wc -l)
+    echo $number_of_Files
 }
 
 function game {
-numberofFiles=$(files)
-loop=1
-echo "Guess the number of files in the current directory"
-while [[ $loop -eq 1 ]]
-do
-
-read -p "Your guess :" guess
-if [[ $guess =~ [^0-9] ]]
-then echo "The value you have entered is not a VALID number"
-elif [[ $guess -eq $numberofFiles ]] 
-then
-echo "Bravo! You have guessed exact number of files"
-loop=0
-elif [[ $guess -gt $numberofFiles ]]
-then
-echo "Sorry but you have guessed too HIGH, try again"
-else
-echo "Sorry but you have guessed too LOW, try again"
-fi
-
-done
+    numberofFiles=$(files)
+    loop=1
+    echo "Guess the number of files in the current directory"
+    
+    while [[ $loop -eq 1 ]]
+        do
+        read -p "Your guess :" guess
+        if [[ $guess =~ [^0-9] ]]
+        then echo "The value you have entered is not a VALID number"
+        elif [[ $guess -eq $numberofFiles ]] 
+        then
+        echo "Bravo! You have guessed exact number of files"
+        loop=0
+        elif [[ $guess -gt $numberofFiles ]]
+        then
+        echo "Sorry but you have guessed too HIGH, try again"
+        else
+        echo "Sorry but you have guessed too LOW, try again"
+        fi
+        done
 }
 
 game
